6f45bb71bfb2e0ad0102fd0af2755870cf644a3d
anomani <anomani@LIN25008038.corp.capgemini.com>
2021-12-08 00:49:30 +0530

    hello

---
+++ FlightBookingSystem_API_Gateway2/src/main/java/com/flight_booking_api_gateway/config/GatewayConfig.java
@@ -0,0 +1,28 @@
+package com.flight_booking_api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaClient
+public class GatewayConfig
+{
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder)
+	{
+		return routeLocatorBuilder.routes()
+				.route(p ->p
+                        .path("/fares/findAll")
+                        .uri("http://localhost:8091"))
+				.route(p ->p
+                        .path("/fares/findAll")
+                        .uri("http://localhost:8091"))
+		       .build();
+		
+		
+	}
+
+}
